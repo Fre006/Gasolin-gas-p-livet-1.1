c3411ecf54e5fd0f42671906ee45a5fd7140a160
Frederick Grønbech Kure Ertel <fre006@edu.zealand.dk>
2025-03-18 12:18:05 +0100

    ugga bugga jeg bestemmer

---
+++ Gasolin gas på livet 1.1/Pages/Kim Larsen.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Gasolin_gas_på_livet_1._1.Pages
+{
+    public class Kim_LarsenModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
