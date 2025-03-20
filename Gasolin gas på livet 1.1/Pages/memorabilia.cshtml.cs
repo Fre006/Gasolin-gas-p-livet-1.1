7813d7a76a5eacb56b873456c734391b5f03811b
Frederick Grønbech Kure Ertel <fre006@edu.zealand.dk>
2025-03-20 07:50:44 +0100

    adding memorabilia page

---
+++ Gasolin gas på livet 1.1/Pages/memorabilia.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Gasolin_gas_på_livet_1._1.Pages
+{
+    public class memorobiliaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
